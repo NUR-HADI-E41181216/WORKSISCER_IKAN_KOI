26c2e629c6dfbfb4028d1e14406e40c9638d8b13
iamTeddyHariyanto <teddy.premadi2000@gmail.com>
2021-07-17 12:30:48 +0700

    cara kerja

--- IkanKoi/application/views/CarakerjaV.php
+++ IkanKoi/application/views/CarakerjaV.php
@@ -19,11 +19,26 @@
                         </div>
                         <div class="card-body" style="margin: 0 auto; text-align:justify">
                             <div class="row">
-                                <h5>1. Geser halam ke paling akhir untuk menemukan tombol untuk Uji Coba Kualitas Air, atau klik menu "Uji Kualitas Air" yang ada di samping hingga muncul halaman Uji Coba Air</h5>
-                                <img src="<?= base_url() ?>/assets/img/c1.jpg" class="rounded float-left" />
-                                <h5>1. Geser halam ke paling akhir untuk menemukan tombol untuk Uji Coba Kualitas Air, atau klik menu "Uji Kualitas Air" yang ada di samping hingga muncul halaman Uji Coba Air</h5>
-                                <img src="<?= base_url() ?>/assets/img/c1.jpg" class="rounded float-left" />
-
+                                <h5 class="mb-4">1. Geser halam ke paling akhir untuk menemukan tombol untuk Uji Coba Kualitas Air, atau klik menu "Uji Kualitas Air" yang ada di samping hingga muncul halaman Uji Coba Air</h5>
+                                <div class="text-center mb-4">
+                                    <img src="<?= base_url() ?>/assets/img/tombol_uji.png" class="rounded float-left" width="600" height="280" />
+                                </div>
+                                <h5 class="mb-4">2. Setelah berada di halaman "Uji Kualitas Air", selanjutnya masukkan nilai data yang sudah didapatkan (Suhu, TDS, dan DO), kemudian pilih tombol "Proses" </h5>
+                                <div class="text-center mb-4">
+                                    <img src="<?= base_url() ?>/assets/img/input_data.png" class="rounded float-left" width="600" height="240" />
+                                </div>
+                                <h5 class="mb-4">3. Hasil perhitungan Uji Kualitas Air akan langsung ditampilkan dan untuk uji kualitas air tiap jenis ikan juga akan ditampilkan. Pilih tombol "Simpan" jika ingin menyimpan data hasil uji kualitas air.</h5>
+                                <div class="text-center mb-4">
+                                    <img src="<?= base_url() ?>/assets/img/hasil.png" class="rounded float-left" width="600" height="300" />
+                                </div>
+                                <h5 class="mb-4">4. Setelah menyimpan data hasil uji kualitas air, data tersebut dapat dilihat pada menu "History".</h5>
+                                <div class="text-center mb-4">
+                                    <img src="<?= base_url() ?>/assets/img/history.png" class="rounded float-left" width="600" height="300" />
+                                </div>
+                                <h5 class="mb-4">5. Pada menu History, ditampilkan data yang pernah diujikan. Pada menu history, juga dapat melihat detail per data lengkap dengan hasil grade pada tiap jenis ikan koi.</h5>
+                                <div class="text-center">
+                                    <img src="<?= base_url() ?>/assets/img/detail_history.png" class="rounded float-left" width="600" height="300" />
+                                </div>
                             </div>
                         </div>
                     </div>
